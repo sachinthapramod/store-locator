2880e940ee38f2df3808b0f9129c0f5966795504
Sachintha <125460323+sachinthapramod@users.noreply.github.com>
2025-04-28 20:31:23 +0530

    final

--- project/server/config.js
+++ project/server/config.js
@@ -3,4 +3,4 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const PORT = process.env.PORT || 5000;
-export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://myAtlasDBUser:Sp20000320@myatlasclusteredu.4rs45yi.mongodb.net/?retryWrites=true&w=majority&appName=myAtlasClusterEDU'; 
+export const MONGODB_URI = process.env.MONGODB_URI || MONGODB_URI here; 
